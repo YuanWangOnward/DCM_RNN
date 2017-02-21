c4a82497b173be8aaf85e4d15c261b28f6f4b422
Yuan Wang <wangyuan.onward@gmail.com>
2017-02-20 20:31:09 -0500

    Add a second template.

--- DCM_RNN/resource_templates.py
+++ DCM_RNN/resource_templates.py
@@ -13,7 +13,7 @@ file_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(file_path + "/../")
 print('working directory is ' + os.getcwd())
 
-# template 1, used in ISMRM2017 abstract
+# template 0, used in ISMRM2017 abstract
 # new and setting
 du = toolboxes.DataUnit()
 du._secured_data['if_random_neural_parameter'] = False
@@ -75,5 +75,56 @@ if IF_LOADING_TEST:
     with open(data_path, 'rb') as f:
         du_load = pickle.load(f)
         np.testing.assert_array_equal(du._secured_data['y'], du_load._secured_data['y'])
+
+# template 1, randomly generated
+du = toolboxes.DataUnit()
+du._secured_data['if_random_node_number'] = False
+du._secured_data['if_random_stimuli'] = True
+du._secured_data['if_random_x_state_initial'] = False
+du._secured_data['if_random_h_state_initial'] = False
+du._secured_data['t_delta'] = 1. /16.
+du._secured_data['t_scan'] = 3 * 60
+du._secured_data['learning_rate'] = 0.1
+du._secured_data['n_backpro'] = 12
+du._secured_data['n_node'] = 3
+du._secured_data['n_stimuli'] = 1
+du.complete_data_unit(if_show_message=False, if_check_property=True)
+
+if IF_CHECK_RESULT:
+    x = du._secured_data['x']
+    h = du._secured_data['h']
+    y = du._secured_data['y']
+
+    x_axis = np.arange(du._secured_data['n_time_point']) * du._secured_data['t_delta']
+    n_node = du._secured_data['n_node']
+
+    plt.figure()
+    for n in range(n_node):
+        value = x[:, n]
+        plt.subplot(n_node, 1, n + 1)
+        plt.plot(x_axis, value)
+
+    plt.figure()
+    for n in range(n_node):
+        value = h[:, n, :].squeeze()
+        plt.subplot(n_node, 1, n + 1)
+        plt.plot(x_axis, value)
+
+    plt.figure()
+    for n in range(n_node):
+        value = y[:, n].squeeze()
+        plt.subplot(n_node, 1, n + 1)
+        plt.plot(x_axis, value)
+
+# save
+data_path = 'DCM_RNN/resources/template1' + '.pkl'
+with open(data_path, 'wb') as f:
+    pickle.dump(du, f)
+
+if IF_LOADING_TEST:
+    with open(data_path, 'rb') as f:
+        du_load = pickle.load(f)
+        np.testing.assert_array_equal(du._secured_data['y'], du_load._secured_data['y'])
+
 
 
