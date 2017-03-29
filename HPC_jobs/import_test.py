db99d4b86da766468ebbfece6ddadfdccf5eee8d
Yuan Wang <wangyuan.onward@gmail.com>
2017-03-29 09:00:37 -0400

    Try to transfer to HPC.

---
+++ HPC_jobs/import_test.py
@@ -0,0 +1,14 @@
+
+
+# import tensorflow as tf
+# import DCM_RNN.tf_model as tfm
+# import DCM_RNN.toolboxes as tb
+# import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pickle
+import datetime
+import warnings
+
+
+print('import test pass')
